c3e4a34be8ecd256118dda78888a05599a83e43e
jakebogan01 <jakebogan01@gmail.com>
2025-08-13 15:46:58 -0400

    created basic pages and added pocketbase package

---
+++ src/lib/pocketbase.js
@@ -0,0 +1,8 @@
+export const ssr = false;
+
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+
+export default pb;
---
+++ src/lib/utils/constants.js
@@ -0,0 +1,7 @@
+// auth
+export const SIGNIN = '/signin';
+export const LOGOUT = '/logout';
+
+// dashboard
+export const DASHBOARD = '/dashboard';
+export const SETTINGS = '/dashboard/settings';
