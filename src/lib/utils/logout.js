3f5448ed1b31a74a9c99e5694bf1df38395d35f1
jakebogan01 <jakebogan01@gmail.com>
2025-08-18 22:11:29 -0400

    small udpdate

--- src/lib/utils/logout.js
+++ src/lib/utils/logout.js
@@ -1,7 +1,6 @@
 import pb from '$lib/pocketbase.js';
 import { toast } from 'svelte-sonner';
 import { goto } from '$app/navigation';
-
 import { SIGNIN } from '$lib/constants.js';
 
 export const logout = async () => {
